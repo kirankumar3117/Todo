6f1f011872f010018a897968730bbc52bd91622a
kiran <ch.kirankumar311@gmail.com>
2022-05-21 15:34:44 +0530

    completed

--- src/components/todos/Todo1.jsx
+++ src/components/todos/Todo1.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import Todo1list from './Todo1list'
 import {v4 as uuidv4} from "uuid"
 
-import {RiDeleteBin5Line} from "react-icons/Ri"
-
 const Todo1 = ({setList1,list1,handleDelete,handleremove}) => {
     const [input,setInput]=useState("")
     const handleChange=(e)=>{
@@ -54,7 +52,10 @@ const Todo1 = ({setList1,list1,handleDelete,handleremove}) => {
                  </div>
                  <div className={e.state==false?'content':'content1'}>{e.value}</div>
                 <div  className='delete'><button onClick={()=>handleremove(e.id)}  className='deletebutton'>
-                  <RiDeleteBin5Line size="30px" className='icondelete'/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="currentColor" class="bi bi-trash icondelete" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>
                   </button></div>
                  </div>
          })}
